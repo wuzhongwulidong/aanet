29e66613fa41a5689a27ad058450a06db0aa0590
wuzhongwulidong <707881742@qq.com>
2021-06-11 09:21:46 +0800

    【Diag_mini_modify_AANet】提交用于kitti_mix训练的脚本，在做kitti相关的训练时，使用对称padding。

--- scripts/Diag_mini_modify_AANet_train.sh
+++ scripts/Diag_mini_modify_AANet_train.sh
@@ -4,46 +4,51 @@
 # Train on Scene Flow training set。注意，nproc_per_node表示所用的GPU个数。
 # val模式:表示训练并且每个epoch结束后都进行验证：train_loader使用“train”数据集，val_loader使用其中的“val”数据集。各数据集文件名见dataloader.py
 #python -m torch.distributed.launch  --nproc_per_node=1 --master_addr=127.0.0.1 --master_port=29501 train.py --distributed
-python train.py \
---mode val \
---debug_overFit_train 1 \
---learning_rate 0.001 \
---accumulation_steps 1 \
---data_dir data/SceneFlow \
---checkpoint_dir checkpoints/Diag_mini_modify_AANet \
---batch_size 6 \
---val_batch_size 1 \
---img_height 288 \
---img_width 576 \
---val_img_height 576 \
---val_img_width 960 \
---feature_type aanet \
---feature_pyramid_network \
---milestones 20,30,40,50,60 \
---max_epoch 100 \
+#echo 'Train on Scene Flow training set.'
+#python train.py \
+#--mode val \
+#--debug_overFit_train 2 \
+#--learning_rate 0.001 \
+#--accumulation_steps 1 \
+#--data_dir data/SceneFlow \
+#--checkpoint_dir checkpoints/Diag_mini_modify_AANet \
+#--batch_size 6 \
+#--val_batch_size 1 \
+#--img_height 288 \
+#--img_width 576 \
+#--val_img_height 576 \
+#--val_img_width 960 \
+#--feature_type aanet \
+#--feature_pyramid_network \
+#--milestones 20,30,40,50,60,75,85 \
+#--max_epoch 100 \
 
 #--milestones 20,30,40,50,60 \
 #echo '# Train on mixed KITTI 2012 and KITTI 2015 training set'
 #
 ## Train on mixed KITTI 2012 and KITTI 2015 training set
-#python train.py \
-#--data_dir data/KITTI \
-#--dataset_name KITTI_mix \
-#--checkpoint_dir checkpoints/aanet_kittimix \
-#--pretrained_aanet checkpoints/aanet_sceneflow/aanet_best.pth \
-#--batch_size 6 \
-#--val_batch_size 6 \
-#--img_height 336 \
-#--img_width 960 \
-#--val_img_height 384 \
-#--val_img_width 1248 \
-#--feature_type aanet \
-#--feature_pyramid_network \
-#--load_pseudo_gt \
-#--milestones 400,600,800,900 \
-#--max_epoch 1000 \
-#--save_ckpt_freq 100 \
-#--no_validate 2>&1 |tee logs/log_train_aanet_train_KITTI_mix.txt
+echo 'Train on mixed KITTI 2012 and KITTI 2015 training set.'
+python train.py \
+--mode train \
+--debug_overFit_train 2 \
+--data_dir data/KITTI \
+--dataset_name KITTI_mix \
+--checkpoint_dir checkpoints/Diag_mini_modify_kittimix \
+--pretrained_aanet checkpoints/Diag_mini_modify_AANet/aanet_best.pth \
+--batch_size 2 \
+--val_batch_size 1 \
+--img_height 336 \
+--img_width 960 \
+--val_img_height 384 \
+--val_img_width 1248 \
+--feature_type aanet \
+--feature_pyramid_network \
+--load_pseudo_gt \
+--milestones 400,600,800,900 \
+--max_epoch 1000 \
+--save_ckpt_freq 100 \
+--no_validate
+## 2>&1 |tee logs/log_train_aanet_train_KITTI_mix.txt
 #
 ## Train on KITTI 2015 training set：利用上述混合训练的结果，进行fine-tune
 #python train.py \
