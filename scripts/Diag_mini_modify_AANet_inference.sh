6d4d089dce8b204240a8e0f103b53d6fe1427852
wuzhongwulidong <707881742@qq.com>
2021-06-27 16:29:44 +0800

    kitti2012测试 用于Benchmark提交

--- scripts/Diag_mini_modify_AANet_inference.sh
+++ scripts/Diag_mini_modify_AANet_inference.sh
@@ -13,30 +13,31 @@
 
 # Inference on KITTI 2015 test set for submission
 #export CUDA_VISIBLE_DEVICES=0
-python inference.py \
---mode test \
---data_dir data/KITTI/kitti_2015/data_scene_flow \
---dataset_name KITTI2015 \
---pretrained_aanet myTrainedModels/aanet_best_d1_61f9a784.pth \
---batch_size 1 \
---img_height 384 \
---img_width 1248 \
---feature_type aanet \
---feature_pyramid_network \
---no_intermediate_supervision \
---output_dir output/kitti15_test
-# 2>&1 | tee myDebug_log.txt
-
-# Inference on KITTI 2012 test set for submission
-#CUDA_VISIBLE_DEVICES=0 python inference.py \
+#python inference.py \
 #--mode test \
-#--data_dir data/KITTI/kitti_2012/data_stereo_flow \
-#--dataset_name KITTI2012 \
-#--pretrained_aanet pretrained/aanet_kitti12-e20bb24d.pth \
+#--data_dir data/KITTI/kitti_2015/data_scene_flow \
+#--dataset_name KITTI2015 \
+#--pretrained_aanet myTrainedModels/aanet_best_d1_61f9a784.pth \
 #--batch_size 1 \
 #--img_height 384 \
 #--img_width 1248 \
 #--feature_type aanet \
 #--feature_pyramid_network \
 #--no_intermediate_supervision \
-#--output_dir output/kitti12_test
+#--output_dir output/kitti15_test
+## 2>&1 | tee myDebug_log.txt
+
+# Inference on KITTI 2012 test set for submission
+#CUDA_VISIBLE_DEVICES=0
+ python inference.py \
+--mode test \
+--data_dir data/KITTI/kitti_2012/data_stereo_flow \
+--dataset_name KITTI2012 \
+--pretrained_aanet myTrainedModels/aanet_best_d1_kitti12_442cb8d4.pth \
+--batch_size 1 \
+--img_height 384 \
+--img_width 1248 \
+--feature_type aanet \
+--feature_pyramid_network \
+--no_intermediate_supervision \
+--output_dir output/kitti12_test
